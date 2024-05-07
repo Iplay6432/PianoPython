80ca58b0bafa9e53403722a690f716da8d49c616
JacobPercy <jacob.percy@icloud.com>
2024-05-07 12:02:16 -0400

    Started the main screen

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 from backend import Note, Color #,SONGS
+import serial
 
 
 class PianoGame:
@@ -47,16 +48,62 @@ class PianoGame:
             self.running = False
 
     def render_frame(self):
-        key_count = len(self.notes)
-        self.keys = []
-        for x in range(0,self.width, 1+(self.width//key_count)): # Need to add function to move certain keys up and down(black keys) 
-            self.keys.append(
-                pygame.Rect(x, 0, self.width//key_count, self.height)  # Can we make them shorter?
-            )
+        if self.gamestate==2:
+            key_count = len(self.notes)
+            self.keys = []
+            for x in range(0,self.width, 1+(self.width//key_count)): # Need to add function to move certain keys up and down(black keys) 
+                self.keys.append(
+                    pygame.Rect(x, 0, self.width//key_count, self.height)  # Can we make them shorter?
+                )
 
-        for key in self.keys:
-            pygame.draw.rect(self.screen, Color.WHITE, key) # Need to add function to change color of certain keys(black keys)
-        
+            for key in self.keys:
+                pygame.draw.rect(self.screen, Color.WHITE, key) # Need to add function to change color of certain keys(black keys)
+        elif self.gamestate == 0:
+            self.screen.fill(Color.BLACK)
+
+            #Render "Raspberry Pi-ano" text
+            font = pygame.font.Font('freesansbold.ttf', 60)
+            title_screen = font.render("Raspberry Pi-ano",True,Color.WHITE)
+            text_rect_1 = title_screen.get_rect(center=(int(self.width/2), 200))
+            self.screen.blit(title_screen,text_rect_1)
+
+
+            #Buttons
+            font = pygame.font.Font('freesansbold.ttf', 25)
+
+            twinkle = font.render(" Twinkle ",True,Color.BLACK)
+            text_rect_2 = twinkle.get_rect(center=(int(self.width/2), 100))
+            pygame.draw.rect(self.screen,Color.WHITE,text_rect_2)
+            self.screen.blit(twinkle,text_rect_2)
+            
+            vivaldi = font.render(" Vivaldi ",True,Color.BLACK)
+            text_rect_3 = vivaldi.get_rect(center=(int(self.width/2)+150, 100))
+            pygame.draw.rect(self.screen,Color.WHITE,text_rect_3)
+            self.screen.blit(vivaldi,text_rect_3)
+
+            fur = font.render(" Fur elise ",True,Color.BLACK)
+            text_rect_4 = fur.get_rect(center=(int(self.width/2)+300, 100))
+            pygame.draw.rect(self.screen,Color.WHITE,text_rect_4)
+            self.screen.blit(fur,text_rect_4)
+
+            birthday = font.render(" Birthday ",True,Color.BLACK)
+            text_rect_5 = fur.get_rect(center=(int(self.width/2)-150, 100))
+            pygame.draw.rect(self.screen,Color.WHITE,text_rect_5)
+            self.screen.blit(birthday,text_rect_5)
+
+            ode = font.render(" Ode Joy ",True,Color.BLACK)
+            text_rect_6 = fur.get_rect(center=(int(self.width/2)-300, 100))
+            pygame.draw.rect(self.screen,Color.WHITE,text_rect_6)
+            self.screen.blit(ode,text_rect_6)
+
+
+
+
+
+
+
+
+
 
 
 def main():
