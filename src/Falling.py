7b04412ab531345bebbd33f4586c00bd0a637213
Iplay6432 <iplay6432bis@gmail.com>
2024-05-17 15:33:13 -0400

    Falling Key list works, though all are falling at once

--- src/Falling.py
+++ src/Falling.py
@@ -2,53 +2,54 @@ import pygame
 from backend import Note, Color
 from key import Key
 import json
+import re
 
 class Falling(pygame.Rect):
-    def __init__ (self, note, screen, height, width,location, len,notes): #note is the index
+    def __init__(
+        self, note, screen, height, width, location, len, notes
+    ):  # note is the index
         # super.__init__(args)
         self.note = note
         self.screen = screen
         self.height = height
-        self.width = width/1.2
+        self.width = width / 1.2
         self.location = location
         self.len = len
-        self.notes= notes
+        self.notes = notes
         self.speed = 5
         with open("jsons/Give.json") as f:
             self.song = json.load(f)
+    
+    
     def place_key(self):
+        def is_float(value):
+            return isinstance(value, float)
+    
         key_count = len(self.notes)
         self.keys = []
         vals = [0, 56, 112, 168, 224, 280, 336, 392, 448, 504, 560, 616]
         note_names = list(Note.__members__.keys())
         for note in self.song:
-            no = [x for x in note[0] if not isinstance(x, int)]
-            print(no)
-            no = note_names.index(no[0])
+            
+            no = ''.join([i for i in note[0] if not i.isdigit()])
+         
+            no = note_names.index(no)
             no = vals[no]
-            print(no)
+
             self.keys.append(
                 Key(
                     Color.GREEN,
                     Note.A,
                     no,
                     0,
-                    (int(self.width)//key_count),
-                    int(self.len))
+                    (int(self.width) // key_count),
+                    int(self.len),
                 )
-        self.keys.append(
-            Key(
-                Color.GREEN,
-                Note.A,
-                vals[self.note],
-                0,
-                (int(self.width)//key_count),
-                int(self.len))
             )
-            
-        
+
         for key in self.keys:
             pygame.draw.rect(self.screen, key.current_color, key)
+
     def update(self):
         for key in self.keys.copy():  # iterate over a copy of the list
             # update y-coordinate
@@ -58,7 +59,7 @@ class Falling(pygame.Rect):
             pygame.draw.rect(self.screen, key.current_color, key)
 
             # remove key if it's off the screen
-            if key.y > self.height/2:
+            if key.y > self.height / 2:
                 self.keys.remove(key)
-        
+
         # pygame.draw.rect(self.screen, Color.GREEN, pygame.Rect((self.note*((self.width/1.2)/13)*2),self.location,((self.width/1.2)/13)*2,self.len))
