11821cf5a58f976234529deceb58db13b7cda866
Iplay6432 <iplay6432bis@gmail.com>
2024-05-22 17:44:07 -0400

    Refactor pin initialization and reading logic

--- Arduino Code/Keys/Keys.ino
+++ Arduino Code/Keys/Keys.ino
@@ -1,20 +1,36 @@
+#define MAX_PINS 7
+#define MAX_PINS 7
+#define START_PIN 2
+
+bool pinsActive[MAX_PINS] = {
+  false,
+  false,
+  false,
+  false,
+  true,
+  false,
+  false,
+};
+
 void setup() {
   Serial.begin(9600);
-  for (int i=2; i < 8; i++){
-    pinMode(i, INPUT);
+  for (int i=START_PIN; i < MAX_PINS+START_PIN; i++){
+    if (pinsActive[i-START_PIN])
+      pinMode(i, INPUT);
   }
 }
 
 void loop() {
-  char output[7];
-  output[7] = '\0';
-  for (int i=2; i < 8; i++) {
-    if (digitalRead(i) == LOW) {
-      output[i-2] = '1';
+  char output[MAX_PINS];
+  output[MAX_PINS-1] = '\0';
+  for (int i=START_PIN; i < MAX_PINS+START_PIN; i++) {
+    const int index = i-START_PIN;
+    if (pinsActive[index] && digitalRead(i) == HIGH) {
+      output[index] = '1';
     } else {
-      output[i-2] = '0';
+      output[index] = '0';
     }
   }
   Serial.println(output);
-  delay(1000);
+  delay(10);
 }
