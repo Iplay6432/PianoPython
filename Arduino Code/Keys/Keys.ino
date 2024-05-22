fcfeb1d0c657cf5475ef882ac1b2814a9a5ea760
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-05-22 11:07:27 -0400

    Fix arduino code

--- Arduino Code/Keys/Keys.ino
+++ Arduino Code/Keys/Keys.ino
@@ -1,3 +1,5 @@
+#include <string>
+
 void setup() {
   Serial.begin(9600);
   for (int i=2; i < 8; i++){
@@ -9,9 +11,9 @@ void loop() {
   std::string output = "";
   for (int i=2; i < 8; i++) {
     if (digitalRead(i) == LOW) {
-      string = string + "1";
+      string += "1";
     } else {
-      string = string + "0";
+      string += "0";
     }
   }
   Serial.println(output);
